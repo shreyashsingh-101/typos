b0c3c9e9eab021da596189602694d9502ca3df63
Shreyash Singh <Rohitsingh9450@hotmail.com>
2023-01-03 01:03:57 +0530

    Navbar component updated

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,22 +1,20 @@
-
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div
-      className="h-14 px-10 xl:mx-52 sm:mx-24 mx-10  mt-5 rounded-xl bg-[#933a3a37] text-white flex items-center justify-center"
-    >
-      <a href="/" className="text-2xl font-bold mr-5 hover:text-red-500">
+    <div className="h-14 px-10 xl:mx-52 sm:mx-24 mx-10  mt-5 rounded-xl bg-[#933a3a37] text-white flex items-center justify-center">
+      <Link href="/" className="text-2xl font-bold mr-5 hover:text-red-500">
         Typos
-      </a>
+      </Link>
 
       <div className="flex items-center justify-center ml-auto">
-        <a href="/Leaderboard" className="text-xl mr-5 hover:text-red-500">
+        <Link href="/Leaderboard" className="text-xl mr-5 hover:text-red-500">
           Leaderboard
-        </a>
+        </Link>
 
-        <a href="/Profile" className="text-xl mr-5 hover:text-red-500">
+        <Link href="/Profile" className="text-xl mr-5 hover:text-red-500">
           Profile
-        </a>
+        </Link>
       </div>
     </div>
   );
